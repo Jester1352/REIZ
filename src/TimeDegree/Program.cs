e5ad9bcb1f3c2a7d0ce64a75b2129bd6955f8d15
dzakh <dzakh@DESKTOP-76QFO57>
2023-02-19 21:15:48 +0200

    Добавьте файлы проекта.

---
+++ src/TimeDegree/Program.cs
@@ -0,0 +1,16 @@
+﻿using TimeDegree.Core;
+using TimeDegreeCalculation;
+
+Console.WriteLine("Enter hours: ");
+var minutes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enters minutes: ");
+var hours = int.Parse(Console.ReadLine());
+
+ICalculate r = new Time(minutes, hours);
+
+Console.WriteLine("Degree between arrows: ");
+Console.WriteLine(r.CalculateDegree());
+
+
+
---
+++ src/TimeDegree/Time.cs
@@ -0,0 +1,28 @@
+﻿using TimeDegree.Core;
+
+namespace TimeDegree;
+
+public class Time : ICalculate
+{
+    public Time(double a, double b)
+    {
+        H = a;
+        M = b;
+    }
+
+    public double H { get; }
+
+    public double M { get; }
+
+    public double CalculateDegree()
+    {
+        var degree = (int)Math.Abs(M * 6 - H * 30);
+        if (degree > 180)
+            degree = 360 - degree;
+        return degree;
+    }
+}
+
+
+
+
